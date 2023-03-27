077a5495a5749f18b0deeaf6a10aaf53a01c5b72
60jong <rudwhd515@gmail.com>
2023-03-27 17:07:37 +0900

    [Feat] : ProductionRequest 엔티티 생성

---
+++ src/main/java/com/sctk/cmc/domain/ProductionRequest.java
@@ -0,0 +1,33 @@
+package com.sctk.cmc.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class ProductionRequest extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "production_request_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Designer designer;
+
+    private String title;
+    private Integer desiredPrice;
+    private String requirement;
+
+    @OneToOne(mappedBy = "productionRequest")
+    private RequestReference reference;
+
+    private Boolean accepted;
+    private int likeCount;
+    private Boolean active;
+}
