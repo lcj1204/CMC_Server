3915ad9e04440f4b32552ae70c1999b9a954b57c
60jong <rudwhd515@gmail.com>
2023-03-27 17:07:11 +0900

    [Feat] : Portfolio 엔티티 생성

--- src/main/java/com/sctk/cmc/domain/Portfolio.java
+++ src/main/java/com/sctk/cmc/domain/Portfolio.java
@@ -1,14 +1,29 @@
 package com.sctk.cmc.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.List;
 
+@Getter
+@NoArgsConstructor
 @Entity
-public class Portfolio {
+public class Portfolio extends BaseTimeEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "portfolio_id")
     private Long id;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "designer_id")
     private Designer designer;
-    private List<PortfolioImg> portfolioImgs = new ArrayList<>();
+
+    @OneToMany(mappedBy = "portfolio")
+    private List<PortfolioImg> portfolioImgs;
+
+    public Portfolio(Designer designer) {
+        this.designer = designer;
+        this.portfolioImgs = new ArrayList<>();
+    }
 }
