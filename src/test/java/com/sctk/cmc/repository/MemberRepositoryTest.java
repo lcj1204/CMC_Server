fcec3b1399579e316f352377c39d17c9607762ee
60jong <rudwhd515@gmail.com>
2023-04-03 16:09:47 +0900

    [Test] : 이름으로 멤버를 검색하는 기능 테스트케이스 구현

--- src/test/java/com/sctk/cmc/repository/MemberRepositoryTest.java
+++ src/test/java/com/sctk/cmc/repository/MemberRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.*;
 
 @Transactional
@@ -18,13 +20,36 @@ class MemberRepositoryTest {
     public void member_저장_테스트() throws Exception {
         Member member = createMember();
 
+        Long returned = memberRepository.save(member);
+
+        assertThat(member.getId()).isEqualTo(returned);
+    }
+
+    @Test
+    public void member_email로_조회() throws Exception {
+        //given
+        String email = "email";
+        Member member = createMemberWithEmail(email);
         memberRepository.save(member);
 
-        assertThat(member.getId()).isEqualTo(1L);
+        //when
+        Member findMember = memberRepository.findByEmail(email)
+                .orElse(null);
+
+        //then
+        assertThat(findMember).isNotNull();
+        assertThat(findMember.getId().equals(member.getId())).isTrue();
+        assertThat(findMember.getEmail().equals(member.getEmail())).isTrue();
     }
 
     public Member createMember() {
         return Member.builder()
+                .build();
+    }
+
+    public Member createMemberWithEmail(String email) {
+        return Member.builder()
+                .email(email)
                 .build();
     }
 }
