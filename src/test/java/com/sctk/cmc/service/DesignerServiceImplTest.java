fdfb54759484cb44a9d3f1744bda76ef961f53fc
60jong <rudwhd515@gmail.com>
2023-04-06 01:14:07 +0900

    [Test] : 상위 카테고리, 하위 카테고리 등록하는 기능 테스트케이스 구현

--- src/test/java/com/sctk/cmc/service/DesignerServiceImplTest.java
+++ src/test/java/com/sctk/cmc/service/DesignerServiceImplTest.java
@@ -1,20 +1,23 @@
 package com.sctk.cmc.service;
 
 import com.sctk.cmc.domain.Designer;
+import com.sctk.cmc.domain.HighCategory;
+import com.sctk.cmc.domain.LowCategory;
 import com.sctk.cmc.dto.designer.DesignerJoinParam;
 import com.sctk.cmc.exception.CMCException;
 import com.sctk.cmc.repository.DesignerRepository;
+import com.sctk.cmc.service.abstractions.DesignerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.util.Collection;
+import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static com.sctk.cmc.exception.ResponseStatus.AUTHENTICATION_ILLEGAL_EMAIL;
+import static com.sctk.cmc.exception.ResponseStatus.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.any;
@@ -126,6 +129,102 @@ class DesignerServiceImplTest {
                 .hasMessage(AUTHENTICATION_ILLEGAL_EMAIL.name());
     }
 
+    @Test
+    public void 상위카테고리_등록_테스트() throws Exception {
+        //given
+        String designerName = "designer";
+        String designerEmail = "email";
+        Designer designer = createDesignerHasNameEmail(designerName, designerEmail);
+        setId(designer, 1L);
+
+        List<HighCategory> highCategories = List.of(
+                new HighCategory(designer, "highCategory1"),
+                new HighCategory(designer, "highCategory2"),
+                new HighCategory(designer, "highCategory3")
+        );
+
+
+        when(designerRepository.findById(any())).thenReturn(Optional.of(designer));
+
+        //when
+        int registered = designerService.registerHighCategories(1L, highCategories);
+
+        //then
+        assertThat(registered).isEqualTo(highCategories.size());
+    }
+
+    @Test
+    public void 상위카테고리_등록_3개_초과로_실패_테스트() throws Exception {
+        //given
+        String designerName = "designer";
+        String designerEmail = "email";
+        Designer designer = createDesignerHasNameEmail(designerName, designerEmail);
+        setId(designer, 1L);
+
+        List<HighCategory> highCategories = List.of(
+                new HighCategory(designer, "highCategory1"),
+                new HighCategory(designer, "highCategory2"),
+                new HighCategory(designer, "highCategory3"),
+                new HighCategory(designer, "highCategory4")
+        );
+
+
+        when(designerRepository.findById(any())).thenReturn(Optional.of(designer));
+
+        //then
+        assertThatThrownBy(() -> designerService.registerHighCategories(1L, highCategories))
+                .isInstanceOf(CMCException.class)
+                .hasMessage(DESIGNERS_HIGH_CATEGORY_MORE_THAN_LIMIT.name());
+    }
+
+    @Test
+    public void 하위카테고리_등록_테스트() throws Exception {
+        //given
+        String designerName = "designer";
+        String designerEmail = "email";
+        Designer designer = createDesignerHasNameEmail(designerName, designerEmail);
+        setId(designer, 1L);
+
+        List<LowCategory> lowCategories = List.of(
+                new LowCategory(designer, "lowCategory1"),
+                new LowCategory(designer, "lowCategory2"),
+                new LowCategory(designer, "lowCategory3")
+        );
+
+
+        when(designerRepository.findById(any())).thenReturn(Optional.of(designer));
+
+        //when
+        int registered = designerService.registerLowCategories(1L, lowCategories);
+
+        //then
+        assertThat(registered).isEqualTo(lowCategories.size());
+    }
+
+    @Test
+    public void 하위카테고리_등록_3개_초과로_실패_테스트() throws Exception {
+        //given
+        String designerName = "designer";
+        String designerEmail = "email";
+        Designer designer = createDesignerHasNameEmail(designerName, designerEmail);
+        setId(designer, 1L);
+
+        List<LowCategory> lowCategories = List.of(
+                new LowCategory(designer, "lowCategory1"),
+                new LowCategory(designer, "lowCategory2"),
+                new LowCategory(designer, "lowCategory3"),
+                new LowCategory(designer, "lowCategory4")
+        );
+
+
+        when(designerRepository.findById(any())).thenReturn(Optional.of(designer));
+
+        //then
+        assertThatThrownBy(() -> designerService.registerLowCategories(1L, lowCategories))
+                .isInstanceOf(CMCException.class)
+                .hasMessage(DESIGNERS_LOW_CATEGORY_MORE_THAN_LIMIT.name());
+    }
+
     Designer createDesigner() {
         return Designer.builder().build();
     }
@@ -135,5 +234,12 @@ class DesignerServiceImplTest {
                 .name(name)
                 .email(email)
                 .build();
+    }
+
+    void setId(Designer designer, Long id) throws NoSuchFieldException, IllegalAccessException {
+        Class<? extends Designer> designerClass = designer.getClass();
+        Field idField = designerClass.getDeclaredField("id");
+        idField.setAccessible(true);
+        idField.set(designer, id);
     }
 }
