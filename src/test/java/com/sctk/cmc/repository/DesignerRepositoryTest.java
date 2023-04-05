fae6dbd71ece8dc3867f3d6006b6dfa096ad9a58
60jong <rudwhd515@gmail.com>
2023-04-05 19:30:24 +0900

    [Test] : Designer를 이름으로 검색하는 기능에 대한 테스트 케이스 구현

--- src/test/java/com/sctk/cmc/repository/DesignerRepositoryTest.java
+++ src/test/java/com/sctk/cmc/repository/DesignerRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,13 +35,45 @@ class DesignerRepositoryTest {
         designerRepository.save(designer);
 
         //when
-        Designer findDesigner = designerRepository.findByName(name)
-                .orElse(null);
+        List<Designer> findDesigners = designerRepository.findAllByName(name);
 
         //then
-        assertThat(findDesigner).isNotNull();
-        assertThat(findDesigner.getId().equals(designer.getId())).isTrue();
-        assertThat(findDesigner.getEmail().equals(designer.getEmail())).isTrue();
+        assertThat(findDesigners.size()).isGreaterThan(0);
+        assertThat(findDesigners).contains(designer);
+    }
+
+    @Test
+    public void designer_name으로_조회_중복있을때() throws Exception {
+        //given
+        String name = "designer";
+        String emailA = "emailA";
+        String emailB = "emailB";
+        Designer designerA = createDesignerWithNameEmail(name, emailA);
+        designerRepository.save(designerA);
+
+        Designer designerB = createDesignerWithNameEmail(name, emailB);
+        designerRepository.save(designerB);
+
+        //when
+        List<Designer> findDesigners = designerRepository.findAllByName(name);
+
+        //then
+        assertThat(findDesigners.size()).isEqualTo(2);
+        assertThat(findDesigners).contains(designerA);
+        assertThat(findDesigners).contains(designerB);
+    }
+
+    @Test
+    public void designer_name으로_조회_결과_없을때() throws Exception {
+        //given
+        String name = "designer";
+
+        //when
+        List<Designer> findDesigners = designerRepository.findAllByName(name);
+
+        //then
+        assertThat(findDesigners.size()).isEqualTo(0);
+        assertThat(findDesigners).isEmpty();
     }
 
     @Test
