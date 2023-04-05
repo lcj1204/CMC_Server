bfdfa5a0b6cc5eb480d033da05c3f6b09135701f
60jong <rudwhd515@gmail.com>
2023-04-05 20:00:37 +0900

    [Feat] : Designer 인증하는 기능 구현

--- src/main/java/com/sctk/cmc/service/AuthService.java
+++ src/main/java/com/sctk/cmc/service/AuthService.java
@@ -1,8 +1,11 @@
 package com.sctk.cmc.service;
 
+import com.sctk.cmc.domain.Designer;
 import com.sctk.cmc.domain.Member;
 
 public interface AuthService {
     Member authenticateMember(String email, String password);
+
+    Designer authenticateDesigner(String email, String password);
 }
 
