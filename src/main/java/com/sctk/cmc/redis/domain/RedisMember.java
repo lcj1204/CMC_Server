d50bf171c110968ce639caa8f39dbe84dd654422
lcj1204 <dlckdwo1204@naver.com>
2023-05-08 16:58:47 +0900

    [Refactor]: 주석 추가

--- src/main/java/com/sctk/cmc/redis/domain/RedisMember.java
+++ src/main/java/com/sctk/cmc/redis/domain/RedisMember.java
@@ -9,12 +9,12 @@ import org.springframework.data.redis.core.TimeToLive;
 import java.util.concurrent.TimeUnit;
 
 @Getter
-@RedisHash("MEMBER")
+@RedisHash("MEMBER") // prefix에 입력될 keyspace
 public class RedisMember {
-    @Id
+    @Id // keyspace:@Id 형태로 키를 저장함.
     private String email;
     private String refreshToken;
-    @TimeToLive(unit = TimeUnit.MILLISECONDS)
+    @TimeToLive(unit = TimeUnit.MILLISECONDS) // Redis 에 유지되는 시간
     private Long expiredTimeMs;
 
     @Builder
