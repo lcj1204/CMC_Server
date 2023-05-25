1a00791aadadcab65020c540b0a78d5230168b7b
lcj1204 <dlckdwo1204@naver.com>
2023-05-25 22:25:41 +0900

    [Feat]: 디자이너 Id 로 삭제되지 않은(active=ture)인 모든 Custom 조회 쿼리 구현

--- src/main/java/com/sctk/cmc/repository/CustomRepository.java
+++ src/main/java/com/sctk/cmc/repository/CustomRepository.java
@@ -2,6 +2,17 @@ package com.sctk.cmc.repository;
 
 import com.sctk.cmc.domain.Custom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CustomRepository extends JpaRepository<Custom, Long> {
+
+    @Query("select c from Custom c " +
+            "join fetch c.member " +
+            "join c.designer d " +
+            "where d.id= :designerId " +
+            "and c.active= true ")
+    List<Custom> findAllByDesignerId(@Param("designerId") Long designerId);
 }
