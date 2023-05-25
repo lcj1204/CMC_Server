106509038bd143668df4c911d49a821a30f90411
lcj1204 <dlckdwo1204@naver.com>
2023-05-25 22:15:55 +0900

    [Feat]: Custom의 상세 정보를 반환하는 응답 Dto 구현

---
+++ src/main/java/com/sctk/cmc/web/dto/custom/CustomGetDetailResponse.java
@@ -0,0 +1,37 @@
+package com.sctk.cmc.web.dto.custom;
+
+import com.sctk.cmc.domain.Custom;
+import com.sctk.cmc.domain.CustomStatus;
+import com.sctk.cmc.domain.Member;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CustomGetDetailResponse {
+    private String title;
+    private Long memberId;
+    private String memberName;
+    private String memberProfileImgUrl;
+    private String highCategory;
+    private String lowCategory;
+    private Integer desiredPrice;
+    private String requirement;
+    private CustomStatus accepted;
+
+    public static CustomGetDetailResponse of(Custom custom) {
+        Member member = custom.getMember();
+
+        return CustomGetDetailResponse.builder()
+                .title(custom.getTitle())
+                .memberId(member.getId())
+                .memberName(member.getName())
+                .memberProfileImgUrl(member.getProfileImgUrl())
+                .highCategory(custom.getHighCategory())
+                .lowCategory(custom.getLowCategory())
+                .desiredPrice(custom.getDesiredPrice())
+                .requirement(custom.getRequirement())
+                .accepted(custom.getAccepted())
+                .build();
+    }
+}
