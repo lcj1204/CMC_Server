ecf4b7a2340b9b3a2a56469186770decfc7bdfa0
lcj1204 <dlckdwo1204@naver.com>
2023-05-25 22:09:46 +0900

    [Feat]: CustomRepository 초기 구현

---
+++ src/main/java/com/sctk/cmc/repository/CustomRepository.java
@@ -0,0 +1,7 @@
+package com.sctk.cmc.repository;
+
+import com.sctk.cmc.domain.Custom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomRepository extends JpaRepository<Custom, Long> {
+}
