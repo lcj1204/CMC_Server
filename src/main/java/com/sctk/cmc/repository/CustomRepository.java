b1d274770fe036b8f8f239c4dda4ebed7dd225a4
lcj1204 <dlckdwo1204@naver.com>
2023-05-25 22:28:08 +0900

    [Feat]: CustomId 로 요청한 Member까지 함께 조회하는 쿼리 구현

--- src/main/java/com/sctk/cmc/repository/CustomRepository.java
+++ src/main/java/com/sctk/cmc/repository/CustomRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CustomRepository extends JpaRepository<Custom, Long> {
 
@@ -15,4 +16,11 @@ public interface CustomRepository extends JpaRepository<Custom, Long> {
             "where d.id= :designerId " +
             "and c.active= true ")
     List<Custom> findAllByDesignerId(@Param("designerId") Long designerId);
+
+    @Query("select c from Custom c " +
+            "join fetch c.member " +
+            "join c.designer " +
+            "where c.id= :customId " +
+            "and c.active= true ")
+    Optional<Custom> findWithMemberById(@Param("customId") Long customId);
 }
