cbbeaaf9e2d28cd6253a03b15efceabe74989d5b
KONKUK\s_rudwhd515 <rudwhd515@gmail.com>
2023-06-02 14:00:40 +0900

    [Test]:

--- src/main/java/com/sctk/cmc/service/member/dto/MemberInfo.java
+++ src/main/java/com/sctk/cmc/service/member/dto/MemberInfo.java
@@ -1,9 +1,13 @@
 package com.sctk.cmc.service.member.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@EqualsAndHashCode
+@NoArgsConstructor
 @AllArgsConstructor
 public class MemberInfo {
     private String name;
