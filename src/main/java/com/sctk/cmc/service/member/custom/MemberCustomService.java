af605b7c5bb729baf521918bc06d7a5fa54e9581
lcj1204 <dlckdwo1204@naver.com>
2023-06-04 18:24:52 +0900

    [Feat] 구매자용 보낸 커스텀 요청 전체 간단조회 기능 추상화

--- src/main/java/com/sctk/cmc/service/member/custom/MemberCustomService.java
+++ src/main/java/com/sctk/cmc/service/member/custom/MemberCustomService.java
@@ -1,6 +1,7 @@
 package com.sctk.cmc.service.member.custom;
 
 import com.sctk.cmc.controller.designer.custom.dto.CustomIdResponse;
+import com.sctk.cmc.controller.member.custom.dto.MemberCustomGetInfoResponse;
 import com.sctk.cmc.domain.Custom;
 import com.sctk.cmc.service.member.custom.dto.CustomRegisterParams;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,6 +10,8 @@ import java.util.List;
 
 public interface MemberCustomService {
     CustomIdResponse register(Long memberId, CustomRegisterParams customRegisterParams, List<MultipartFile> multipartFiles);
+
+    List<MemberCustomGetInfoResponse> retrieveAllInfo(Long memberId);
 
     Custom retrieveById(Long customId);
 
