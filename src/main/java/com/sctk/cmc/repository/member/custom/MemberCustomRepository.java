6a309098e585b6ed4a3e6d8639f22b336d2241e9
lcj1204 <dlckdwo1204@naver.com>
2023-06-04 18:25:42 +0900

    [Feat] 커스텀 간단조회 쿼리 구현

--- src/main/java/com/sctk/cmc/repository/member/custom/MemberCustomRepository.java
+++ src/main/java/com/sctk/cmc/repository/member/custom/MemberCustomRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberCustomRepository extends JpaRepository<Custom, Long> {
@@ -24,4 +25,13 @@ public interface MemberCustomRepository extends JpaRepository<Custom, Long> {
             "where c.id= :customId " +
             "and c.active= true ")
     Optional<Custom> findWithMemberAndImgsById(@Param("customId") Long customId);
+
+    @Query("select distinct c from Custom c " +
+            "join fetch c.designer d " +
+            "join fetch c.reference cr " +
+            "join fetch cr.referenceImgs " +
+            "join c.member m " +
+            "where m.id= :memberId " +
+            "and c.active= true ")
+    List<Custom> findAllByMemberId(@Param("memberId") Long memberId);
 }
