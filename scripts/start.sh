fcdda9f2f8e99556c67e7245b6ce15f665822c66
KONKUK\s_rudwhd515 <rudwhd515@gmail.com>
2023-06-07 19:38:18 +0900

    CICD : CI 작성

---
+++ scripts/start.sh
@@ -0,0 +1,15 @@
+PROJECT_ROOT="/home/ubuntu/cmc/artifact"
+JAR_FILE="$PROJECT_ROOT/build/libs/*.jar"
+
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+# jar 파일 실행
+echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
+nohup java -jar $JAR_FILE --spring.profiles.active=develop > $APP_LOG 2> $ERROR_LOG &
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
