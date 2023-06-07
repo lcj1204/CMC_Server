1b97b33d744d80102b75e44f926876d55c55d390
lcj1204 <dlckdwo1204@naver.com>
2023-06-07 17:24:49 +0900

    [Feat] 승인, 거절된 CustomResult 조회 API 구현

--- src/main/java/com/sctk/cmc/controller/member/custom/MemberCustomController.java
+++ src/main/java/com/sctk/cmc/controller/member/custom/MemberCustomController.java
@@ -3,7 +3,10 @@ package com.sctk.cmc.controller.member.custom;
 import com.sctk.cmc.auth.domain.SecurityMemberDetails;
 import com.sctk.cmc.common.response.BaseResponse;
 import com.sctk.cmc.controller.designer.custom.dto.CustomIdResponse;
+import com.sctk.cmc.controller.member.custom.dto.MemberCustomGetDetailResponse;
 import com.sctk.cmc.controller.member.custom.dto.MemberCustomGetInfoResponse;
+import com.sctk.cmc.controller.member.custom.dto.MemberCustomResultGetAcceptanceResponse;
+import com.sctk.cmc.controller.member.custom.dto.MemberCustomResultGetRejectionResponse;
 import com.sctk.cmc.service.member.custom.MemberCustomService;
 import com.sctk.cmc.service.member.custom.dto.CustomRegisterParams;
 import io.swagger.v3.oas.annotations.Operation;
@@ -37,11 +40,46 @@ public class MemberCustomController {
     }
 
     @GetMapping("/custom")
-    @Operation(summary = "커스텀 요청 전체 간단조회 API", description = "자신이 보낸 모든 요청들을 간단 조회합니다.")
+    @Operation(summary = "커스텀 요청 전체 간단 조회 API", description = "구매자가 자신이 보낸 모든 요청들을 간단 조회합니다.")
     public BaseResponse<List<MemberCustomGetInfoResponse>> retrieveAllInfo(@AuthenticationPrincipal SecurityMemberDetails memberDetails) {
 
         Long memberId = memberDetails.getId();
         List<MemberCustomGetInfoResponse> response = memberCustomService.retrieveAllInfo(memberId);
+
+        return new BaseResponse<>(response);
+    }
+
+    @GetMapping("/custom/{customId}/detail")
+    @Operation(summary = "커스텀 요청 상세 조회 API", description = "구매자가 자신이 보낸 커스텀 요청을 상세 조회할 때 사용합니다.")
+    public BaseResponse<MemberCustomGetDetailResponse> retrieveDetail(@AuthenticationPrincipal SecurityMemberDetails memberDetails,
+                                                                      @PathVariable("customId") Long customId) {
+
+        Long memberId = memberDetails.getId();
+        MemberCustomGetDetailResponse response = memberCustomService.retrieveDetailById(memberId, customId);
+
+        return new BaseResponse<>(response);
+    }
+
+    @GetMapping("/custom/{customId}/custom-result/{customResultId}/acceptance")
+    @Operation(summary = "커스텀 요청 승인 결과 조회 API", description = "구매자가 자신이 보낸 커스텀 요청의 승인 결과를 조회할 때 사용합니다.")
+    public BaseResponse<MemberCustomResultGetAcceptanceResponse> retrieveAcceptance(@AuthenticationPrincipal SecurityMemberDetails memberDetails,
+                                                                                    @PathVariable("customId") Long customId,
+                                                                                    @PathVariable("customResultId") Long customResultId) {
+
+        Long memberId = memberDetails.getId();
+        MemberCustomResultGetAcceptanceResponse response = memberCustomService.retrieveAcceptance(memberId, customId, customResultId);
+
+        return new BaseResponse<>(response);
+    }
+
+    @GetMapping("/custom/{customId}/custom-result/{customResultId}/rejection")
+    @Operation(summary = "커스텀 요청 거절 결과 조회 API", description = "구매자가 자신이 보낸 커스텀 요청의 거절 결과를 조회할 때 사용합니다.")
+    public BaseResponse<MemberCustomResultGetRejectionResponse> retrieveRejection(@AuthenticationPrincipal SecurityMemberDetails memberDetails,
+                                                                                           @PathVariable("customId") Long customId,
+                                                                                           @PathVariable("customResultId") Long customResultId) {
+
+        Long memberId = memberDetails.getId();
+        MemberCustomResultGetRejectionResponse response = memberCustomService.retrieveRejection(memberId, customId, customResultId);
 
         return new BaseResponse<>(response);
     }
