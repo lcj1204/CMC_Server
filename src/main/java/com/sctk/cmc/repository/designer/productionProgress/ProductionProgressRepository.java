58bbceab7af111804bc3f40471eb37a39a547cbb
lcj1204 <dlckdwo1204@naver.com>
2023-06-20 13:02:06 +0900

    [Refactor] Production_Progress 레포지토리 경로 재변경

--- src/main/java/com/sctk/cmc/repository/designer/productionProgress/ProductionProgressRepository.java
+++ src/main/java/com/sctk/cmc/repository/designer/productionProgress/ProductionProgressRepository.java
@@ -1,4 +1,4 @@
-package com.sctk.cmc.repository.common.productionProgress;
+package com.sctk.cmc.repository.designer.productionProgress;
 
 import com.sctk.cmc.domain.ProductionProgress;
 import org.springframework.data.jpa.repository.JpaRepository;
