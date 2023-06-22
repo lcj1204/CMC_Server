3e3cae183cbb49084cc3c606df1aeaa01f2a865e
lcj1204 <dlckdwo1204@naver.com>
2023-06-22 20:09:15 +0900

    [Feat] 구매자가 해당 상품에 좋아요 기록 여부 조회하는 기능 구현

---
+++ src/main/java/com/sctk/cmc/service/member/likeProduct/LikeProductServiceImpl.java
@@ -0,0 +1,20 @@
+package com.sctk.cmc.service.member.likeProduct;
+
+import com.sctk.cmc.repository.member.likeProduct.LikeProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Slf4j
+public class LikeProductServiceImpl implements LikeProductService{
+    private final LikeProductRepository likeProductRepository;
+
+    @Override
+    public boolean checkLikeProduct(Long memberId, Long productId) {
+        return likeProductRepository.existsByMemberIdAndId(memberId, productId);
+    }
+}
