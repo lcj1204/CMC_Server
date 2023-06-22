53c297636d21103d5e636d75925107dc62e92ba0
lcj1204 <dlckdwo1204@naver.com>
2023-06-22 16:59:42 +0900

    [Feat] 디자이너용 Product 전체 조회 API

--- src/main/java/com/sctk/cmc/service/designer/Product/DesignerProductService.java
+++ src/main/java/com/sctk/cmc/service/designer/Product/DesignerProductService.java
@@ -1,5 +1,6 @@
 package com.sctk.cmc.service.designer.Product;
 
+import com.sctk.cmc.domain.Product;
 import com.sctk.cmc.service.designer.Product.dto.DesignerProductGetDetailResponse;
 import com.sctk.cmc.service.designer.Product.dto.DesignerProductIdResponse;
 import com.sctk.cmc.service.designer.Product.dto.DesignerProductRegisterParams;
@@ -9,19 +10,13 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 public interface DesignerProductService {
-    // 상품 등록
     DesignerProductIdResponse register(Long designerId,
                                        DesignerProductRegisterParams designerProductRegisterParams,
                                        List<MultipartFile> multipartFileList);
 
-    // 상품 전체 조회
     List<ProductGetInfoResponse> retrieveAllInfoByDesignerId(Long designerId);
 
-    // 상품 상세 조회
     DesignerProductGetDetailResponse retrieveDetailById(Long designerId, Long productId);
 
-    // 상품 수정
-
-    // 상품 삭제
-
+    List<Product> retrieveAllByDesignerId(Long designerId);
 }
--- src/main/java/com/sctk/cmc/service/designer/Product/dto/ProductGetInfoResponse.java
+++ src/main/java/com/sctk/cmc/service/designer/Product/dto/ProductGetInfoResponse.java
@@ -1,4 +1,19 @@
 package com.sctk.cmc.service.designer.Product.dto;
 
+import com.sctk.cmc.domain.Product;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class ProductGetInfoResponse {
+    private Long productId;
+    private String mainImgUrl;
+
+    public static ProductGetInfoResponse of(Product product) {
+        return ProductGetInfoResponse.builder()
+                .productId(product.getId())
+                .mainImgUrl(product.getImgs().get(0).getUrl())
+                .build();
+    }
 }
